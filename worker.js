f774efde2730e6aa6426e8adc558bcece82cb8d1
XNJNP <kolanpoletaev@yandex.ru>
2023-11-27 15:24:11 +0300

    first commit

---
+++ worker.js
@@ -0,0 +1,54 @@
+const OFFLINE_VERSION = 1
+const CACHE_NAME = "offline"
+const OFFLINE_URL = "offline.html"
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        (async () => {
+            const cache = await caches.open(CACHE_NAME)
+
+            await cache.addAll([
+                new Request(OFFLINE_URL, { cache: "reload"}),
+                './script.js',
+                './worker.js'
+            ])
+        })()
+    )
+    self.skipWaiting()
+})
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        (async () =>{
+            if("navigationPreload" in self.registration) {
+                await self.registration.navigationPreload.enable()
+            }
+        })()
+    )
+    self.clients.claim()
+})
+self.addEventListener("fetch", (event) => {
+    if (event.request.mode === "navigate") {
+        event.respondWith(
+            (async () => {
+                try {
+                    const preloadResponse = await event.preloadResponse
+                    if (preloadResponse) {
+                        return preloadResponse
+                    }
+
+                    const networkResponse = await fetch(event.request)
+                    return networkResponse
+                } catch (error) {
+                    console.log("Fetch failed; returning offline page instead.", error)
+
+                    const cache = await caches.open(CACHE_NAME)
+                    const cachedResponse = await cache.match(OFFLINE_URL)
+                    console.log(cachedResponse)
+                    return cachedResponse
+                }
+            })()
+        )
+    }
+})
+
